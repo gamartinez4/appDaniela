c8edb355cb41af62feeee326e0388d99c9a64883
Gabri <gamartinez4@hotmail.com>
2023-01-03 21:34:18 -0500

    se corrigio la version del pagingdata

--- app/src/main/java/com/example/appdaniela/pagerSources/GitRepoPagerSource.kt
+++ app/src/main/java/com/example/appdaniela/pagerSources/GitRepoPagerSource.kt
@@ -2,6 +2,7 @@ package com.example.appdaniela.pagerSources
 
 import android.util.Log
 import androidx.paging.PagingSource
+import androidx.paging.PagingState
 import com.example.appdaniela.models.GitRepListInfo
 import com.example.appdaniela.models.GitRepModel
 import com.example.appdaniela.models.Result
@@ -37,6 +38,13 @@ class GitRepoPagerSource (
         } catch (e: Exception) {
             Log.e("servicio",e.toString())
             LoadResult.Error(e)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, GitRepListInfo>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
+                ?: state.closestPageToPosition(anchorPosition)?.nextKey?.minus(1)
         }
     }
 }
